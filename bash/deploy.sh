d04874c623b54e6d69c85a54bda7278fde5ee636
platocrat <jack.abe.winfield@gmail.com>
2025-06-06 22:39:18 -0500

    fix: remove comments

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-koyeb deploy . \ 
-  postmark-email-worker/worker \  # Creates (or re‑uses) the App postmark-email-worker and a Service called worker.
-  --type worker \   # Marks the service as a long‑running background job (no ports exposed).
-  --instance-type free \  # Pins it to the 512 MB free instance.
-  --regions fra \  # Free tier only allows FRA or IAD.
-  --env CLOUDAMQP_URL=$CLOUDAMQP_URL \  # Injects your RabbitMQ connection string.
+koyeb deploy . postmark-email-worker/worker \
+  --type worker \
+  --instance-type free \
+  --regions fra \
+  --env CLOUDAMQP_URL=$CLOUDAMQP_URL \
   --archive-builder docker \
-  --archive-docker-dockerfile Dockerfile  # Build with the Dockerfile you already have.
+  --archive-docker-dockerfile Dockerfile
