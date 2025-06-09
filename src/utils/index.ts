7bcb365d21f8ba5d3b3b57425bf0c192733ea0d9
Jack Winfield <37757724+platocrat@users.noreply.github.com>
2025-06-09 13:03:33 -0500

    Move fetchJson to api utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,26 +3,4 @@ export * from './misc'
 export * from './api'
 export * from './demo'
 
-export async function fetchJson<T>(
-  url: string,
-  options: RequestInit = {},
-): Promise<T> {
-  const response = await fetch(url, {
-    headers: {
-      'Content-Type': 'application/json',
-      ...(options.headers || {}),
-    },
-    ...options,
-  })
-
-  const data = await response.json()
-
-  if (!response.ok) {
-    const errorMessage =
-      (data as any).error || `Request failed with status ${response.status}`
-    throw new Error(errorMessage)
-  }
-
-  return data as T
-}
 
